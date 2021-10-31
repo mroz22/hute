50bd6d85cee586b4d06b37b8e650c9ad964f95d1
mroz <mvarmuza@gmail.com>
2021-10-31 16:23:46 +0100

    r

--- scripts/config/rewards.ts
+++ scripts/config/rewards.ts
@@ -3,17 +3,27 @@ import type { Reward } from '../../src/types';
 export const REWARDS: Reward[] = [
     {
         name: 'Vysilacky',
-        description: 'popis',
+        description: 'Nalezli jste vysilacky. Od teto chvile muzete pouzivat vase mobily pro komunikaci s ostatnimi agenty',
         cost: 1,
     },
     {
         name: 'Civilni obleceni',
-        description: '',
+        description: 'Pokud mate na sobe vezenske mundury (reflexni vesty), budete si je moci sundat.',
         cost: 2,
     },
     {
         name: 'Spion v marnici',
-        description: '',
+        description: 'Na tomto miste se budou objevovat informace o padlych agentech od nasi krysy pracujici marnici',
         cost: 3,
     },
+    {
+        name: 'Spion na velitelstvi',
+        description: 'Na tomto miste se budou objevovat informace o patrolach cholery',
+        cost: 3,
+    },
+    // {
+    //     name: '',
+    //     description: '',
+    //     cost: 3,
+    // },
 ]
