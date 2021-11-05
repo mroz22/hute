6bf4a8dff22d1f53695b3a91c657c3ca93adda90
mroz <mvarmuza@gmail.com>
2021-11-05 11:01:33 +0100

    uklid

--- scripts/config/points.ts
+++ scripts/config/points.ts
@@ -52,221 +52,141 @@ export const POINTS: Point[] = [
     },
     {
         name: 'Most vedle Al-Nasralláhovy dači',
-        description: 'Pod mostem vedle Al-Nasralláhovy dači hledejte schované kódové označení.', 
+        description: 'Pod mostem vedle Al-Nasralláhovy dači hledejte schované kódové označení.',
         geo: '49.1400325N, 17.2910400E',
         code: 'ohnostroj',
         resolved: false,
     },
     {
 
-    name: 'Dub na mape pod Barborkou',
+        name: 'Dub na mape pod Barborkou',
 
-    description: 'Na vaší mapě naleznete pod bodem Sv. Barborky dub, ležící na červené stezce. Za ním hledejte a najdete.',
+        description: 'Na vaší mapě naleznete pod bodem Sv. Barborky dub, ležící na červené stezce. Za ním hledejte a najdete.',
 
-    geo: ’49.1050839 N,
-    17.3234144 E’,
-
-    code: '',
-
-    resolved: false,
-
-},
-
-{
-
-    name: 'Buchlov',
-
-    description: 'Muzeum lesnictví a myslivosti  na cestě na Buchlov',
-
-    geo: ’49.1141592 N,
-    17.3016294 E’,
+        geo: '49.1050839 N,
+    17.3234144 E',
 
     code: '',
 
-    resolved: false,
-
-},
-
-{
-
-    name: 'Rozcestník nové louky.',
-
-    description: 'Na vaší mapě hledejte v kvadrantu 44/45 - 69/70. Tam, kde leží rozcestí fialové cesty a vede i žlutá, v oblasti Nová louka, najdete kódové označení, které hledáte.',
-
-    geo: ’49.1307783 N,
-    17.3170978 E’,
-
-    code: 'marcipan',
-
-    resolved: false,
-
-}, {
-
-    name: 'posed jelení louka',
-
-    description: '',
-
-    geo: ’49.1377492 N,
-    17.2943339 E’,
-
-    code: 'tomahawk',
-
-    resolved: false,
-
-}, {
-
-    name: 'rozcestník pod mořdířkou',
-
-    description: '',
-
-    geo: ’49.1462733 N,
-    17.2704514 E’,
-
-    code: 'samuraj',
-
-    resolved: false,
-
-}, {
-
-    name: 'tomeškův db',
-
-    description: '',
-
-    geo: ’49.1241236 N,
-    17.2875989 E’,
-
-    code: 'kuratko',
-
-    resolved: false,
-
-}, {
-
-    name: 'pomník u padlých',
-
-    description: '',
-
-    geo: ’49.1499125 N,
-    17.3314958 E’,
-
-    code: '90210',
-
-    resolved: false,
-
-}, {
-
-    name: 'první most na mapě u sh směrem ze stupavy',
-
-    description: '',
-
-    geo: ’49.1256786 N,
-    17.2678150 E’,
-
-    code: 'glabazna',
-
-    resolved: false,
-
-}, {
-
-    name: 'zastávka staré hutě',
-
-    description: '',
-
-    geo: ’49.1255419 N,
-    17.2781306 E’,
-
-    code: 'severka',
-
-    resolved: false,
-
-}, {
-
-    name: 'sklep u malíkovi lípy',
-
-    description: '',
-
-    geo: ’49.1389361 N,
-    17.2864992 E’,
-
-    code: 'hurvinek',
-
-    resolved: false,
-
-}, {
-
-    name: 'samota',
-
-    description: '',
-
-    geo: ’49.1374575 N,
-    17.2482361 E’,
-
-    code: 'pokladhledejzarozhlednou',
-
-    resolved: false,
-
-}, {
-
-    name: 'dubový díl',
-
-    description: '',
-
-    geo: ’49.1304308 N,
-    17.3352361 E’,
-
-    code: 'marlboro',
-
-    resolved: false,
-
-}, {
-
-    name: 'zikmundov rybník',
-
-    description: '49.1297886N, 17.2910869E',
-
-    geo: ’’,
-
-    code: 'ropucha',
-
-    resolved: false,
-
-}, {
-
-    name: 'malý buchlovšutr',
-
-    description: '49.1368200N, 17.3017731E',
-
-    geo: ’’,
-
-    code: 'Josef',
-
-    resolved: false,
-
-}, {
-
-    name: 'diana',
-
-    description: '',
-
-    geo: ’49.1454472 N,
-    17.3053417 E’,
-
-    code: 'desitka',
-
-    resolved: false,
-
-}, {
-
-    name: '',
-
-    description: '',
-
-    geo: ’’,
-
-    code: 'kunovjan',
-
-    resolved: false,
-
-},,
+        resolved: false,
+
+    },
+
+    {
+
+        name: 'Buchlov',
+
+        description: 'Muzeum lesnictví a myslivosti  na cestě na Buchlov',
+
+        geo: '49.1141592 N,
+    17.3016294 E',
+
+    code: '',
+
+        resolved: false,
+
+    },
+
+    {
+
+        name: 'Rozcestník nové louky.',
+        description: 'Na vaší mapě hledejte v kvadrantu 44/45 - 69/70. Tam, kde leží rozcestí fialové cesty a vede i žlutá, v oblasti Nová louka, najdete kódové označení, které hledáte.',
+        geo: '49.1307783 N, 17.3170978 E',
+        code: 'marcipan',
+        resolved: false,
+
+    }, {
+
+        name: 'posed jelení louka',
+        description: '',
+        geo: '49.1377492 N, 17.2943339 E',
+        code: 'tomahawk',
+        resolved: false,
+
+    }, {
+
+        name: 'rozcestník pod mořdířkou',
+        description: '',
+        geo: '49.1462733 N, 17.2704514 E',
+        code: 'samuraj',
+        resolved: false,
+
+    }, {
+
+        name: 'tomeškův db',
+        description: '',
+        geo: '49.1241236 N, 17.2875989 E',
+        code: 'kuratko',
+        resolved: false,
+
+    }, {
+
+        name: 'pomník u padlých',
+        description: '',
+        geo: '49.1499125 N, 17.3314958 E',
+        code: '90210',
+        resolved: false,
+    }, {
+
+        name: 'první most na mapě u sh směrem ze stupavy',
+        description: '',
+        geo: '49.1256786 N, 17.2678150 E',
+        code: 'glabazna',
+        resolved: false,
+
+    }, {
+
+        name: 'zastávka staré hutě',
+        description: '',
+        geo: '49.1255419 N, 17.2781306 E',
+        code: 'severka',
+        resolved: false,
+    }, {
+
+        name: 'sklep u malíkovi lípy',
+        description: '',
+        geo: '49.1389361 N, 17.2864992 E',
+        code: 'hurvinek',
+        resolved: false,
+    }, {
+        name: 'samota',
+        description: '',
+        geo: '49.1374575 N, 17.2482361 E',
+        code: 'pokladhledejzarozhlednou',
+        resolved: false,
+
+    }, {
+        name: 'dubový díl',
+        description: '',
+        geo: '49.1304308 N, 17.3352361 E',
+        code: 'marlboro',
+        resolved: false,
+    }, {
+        name: 'zikmundov rybník',
+        description: '49.1297886N, 17.2910869E',
+        geo: '',
+        code: 'ropucha',
+        resolved: false,
+    }, {
+        name: 'malý buchlovšutr',
+        description: '49.1368200N, 17.3017731E',
+        geo: '',
+        code: 'Josef',
+        resolved: false,
+    }, {
+        name: 'diana',
+        description: '',
+        geo: '49.1454472 N, 17.3053417 E',
+        code: 'desitka',
+        resolved: false,
+
+    }, {
+        name: '',
+        description: '',
+        geo: '',
+        code: 'kunovjan',
+        resolved: false,
+    },
     {
         name: '',
         description: '',
